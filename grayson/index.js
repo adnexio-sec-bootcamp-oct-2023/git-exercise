5b26a434814ad3fb55fa081529a31b620d2efd01
hochangyong <hochangyong1@gmail.com>
2023-11-15 21:32:31 +0800

    done queue 2 grayson

--- grayson/index.js
+++ grayson/index.js
@@ -2,5 +2,6 @@ function convert_myr_to_krw(myr) {
   return myr * 277.72;
 }
 
+
 var krw = convert_myr_to_krw(1000);
-console.log('The currency in krw is : ', krw);
+console.log('The currency in KRW is : ', krw);
